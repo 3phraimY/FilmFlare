f5e12f7abe240d556994220b23923178435ef7fc
lwitte44 <soccerdubbs@gmail.com>
2024-11-12 13:30:28 -0600

    added get all users function

--- Node-API/Routes/users.js
+++ Node-API/Routes/users.js
@@ -17,9 +17,13 @@ router.post("/adduser", async (req, res) => {
 });
 
 //GET all users
-
+router.get("/alluserdata", async (req, res) => { 
+  console.log("Trying to get alluserdata");  
+  const AllUserData = await UserData.find();   
+  res.status(200).json(AllUserData);  
+});
 //GET single user
-
+    //search by username
 //PATCH single user
 
 //DELETE single user
