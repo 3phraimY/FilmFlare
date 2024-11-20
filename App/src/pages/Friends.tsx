99acea12bd7d7dbebe150d47c1cd8ed75c2d8927
3phraimY <ephraim.young@cuw.edu>
2024-11-20 13:56:37 -0600

    created friends page template

--- App/src/pages/Friends.tsx
+++ App/src/pages/Friends.tsx
@@ -1,7 +1,18 @@
+import "./Friends.css";
+
 function Friends() {
   return (
     <>
-      <div>Friends</div>
+      <div className="friends-border-wrapper">
+        <div className="current-friends-wrapper">
+          <div className="current-friends-title">Current Friends</div>
+          <div className="current-friends-content">content</div>
+        </div>
+        <div className="recommended-friends-wrapper">
+          <div className="recommended-friends-title">Recommended Friends</div>
+          <div className="recommended-friends-content">content</div>
+        </div>
+      </div>
     </>
   );
 }
