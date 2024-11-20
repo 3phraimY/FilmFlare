8404578bca45c6b032615021679831ce5c48a190
DranzerZERogue56 <146140740+DranzerZERogue56@users.noreply.github.com>
2024-11-19 21:45:01 -0600

    gradsg

--- App/src/pages/components/Search.tsx
+++ App/src/pages/components/Search.tsx
@@ -6,4 +6,12 @@ function Search() {
   );
 }
 
+/*
+is a Seperate page. that lets you search for movies. 
+call functions from hook file(movie.api)
+we search for 10 movies at first. 
+convert all of those ten items to ten different movie interfaces. (userdatacontext.)
+iterate through all of them and create a tile that takens in the movie interface as a perameter. 
+also be able to include the overlay for the moviedetails page. 
+*/
 export default Search;
