f8677b7ddcc3a1928d544baaf4019e7265b4a659
DranzerZERogue56 <146140740+DranzerZERogue56@users.noreply.github.com>
2024-11-19 21:47:29 -0600

    comment for search

--- App/src/pages/components/Search.tsx
+++ App/src/pages/components/Search.tsx
@@ -12,6 +12,6 @@ call functions from hook file(movie.api)
 we search for 10 movies at first. 
 convert all of those ten items to ten different movie interfaces. (userdatacontext.)
 iterate through all of them and create a tile that takens in the movie interface as a perameter. 
-also be able to include the overlay for the moviedetails page. 
+display movie tile component for each of the 10 search results. 
 */
 export default Search;
