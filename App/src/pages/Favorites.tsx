b9b0ce65deb5fcdc3233f038abd3309b0aac361f
3phraimY <ephraim.young@cuw.edu>
2024-11-21 21:35:07 -0600

    All lists finished

--- App/src/pages/Favorites.tsx
+++ App/src/pages/Favorites.tsx
@@ -1,12 +1,36 @@
-import { useState } from "react";
-import Search from "./components/Search";
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserDataContext";
+import { useNavigate } from "react-router-dom";
+import MovieTile from "./components/MovieTile";
+import "./ToWatch.css";
 
 function Favorites() {
-  const [isSearchActive, setIsSearchActive] = useState<boolean>(false);
+  const context = useContext(UserContext);
+  if (!context) {
+    return <div>Loading...</div>;
+  }
+  const { user } = context;
+  const navigate = useNavigate();
   return (
     <>
-      {isSearchActive && <Search />}
-      <div>Favorites</div>
+      <div className="add-movie">
+        <button onClick={() => navigate("/search")} style={{ display: "flex" }}>
+          <div className="add-movie-text" style={{ alignSelf: "center" }}>
+            Add Movie
+          </div>
+          <img
+            style={{ alignSelf: "center", marginLeft: "5px" }}
+            height={25}
+            width={25}
+            src="https://img.icons8.com/?size=100&id=114100&format=png&color=000000"
+          />
+        </button>
+      </div>
+      <div className="movie-list">
+        {user?.MyFavorites.map((movie) => (
+          <MovieTile movie={movie} />
+        ))}
+      </div>
     </>
   );
 }
